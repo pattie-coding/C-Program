771eb6f9c0ab3fa00a6f71faa7952b3515466269
pattie-coding <pattiecoding@gmail.com>
2026-08-03 00:06:33 +0530

    Largest Number Hehe

--- hello.c
+++ hello.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int main () {
-    printf("Hello Manasvi");
+    int myNum1, myNum2;
+    printf("Enter Your First Number:");
+    scanf("%d", &myNum1);
 
+    printf("Enter Your Second Number:");
+    scanf("%d", &myNum2);
+
+    if(myNum1>myNum2) {
+        printf( "Largest Number = %d", myNum1);
+    }
+
+    else {
+        printf("Largest Number = %d", myNum2);
+    }
 return 0;
 }
